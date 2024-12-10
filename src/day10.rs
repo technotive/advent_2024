79854a30421550abdc534f1eccf102dbe4fc97e0
Nevernown <nevernown@Epsilon.technotive.nl>
2024-12-10 23:00:47 +0100

    ez speed

--- src/day10.rs
+++ src/day10.rs
@@ -17,9 +17,10 @@ pub fn part1(input: &str) -> usize {
     }
     
     let mut sum = 0;
+    let mut seen = HashSet::new();
     for peak in peaks {
         let mut descent = vec![peak];
-        let mut seen = HashSet::new();
+        seen.clear();
         while let Some((row, col)) = descent.pop() {
             if map[row][col] == 0 { seen.insert((row, col)); continue; }
             let target = map[row][col]-1;
@@ -45,14 +46,12 @@ pub fn part1(input: &str) -> usize {
 #[aoc(day10,part2)]
 pub fn part2(input: &str) -> usize {
     let mut peaks = vec![];
-    let mut trial_heads = vec![];
     #[allow(invalid_value)]
     let mut map = [[unsafe { MaybeUninit::<u32>::uninit().assume_init() }; D]; D];
 
     for (row, line) in input.lines().enumerate() {
         for (col, ch) in line.char_indices() {
             map[row][col] = ch as u32 - 48;
-            if map[row][col] == 0 { trial_heads.push((row, col)); }
             if map[row][col] == 9 { peaks.push((row, col)); }
         }
     }
@@ -62,7 +61,7 @@ pub fn part2(input: &str) -> usize {
         let mut walked = [[0; D]; D];
         let mut descent = vec![peak];
         while let Some((row, col)) = descent.pop() {
-            if map[row][col] == 0 { continue; }
+            if map[row][col] == 0 { sum += 1; continue; }
             let target = map[row][col]-1;
             if row > 0 && map[row-1][col] == target {
                 walked[row-1][col] += 1;
@@ -81,8 +80,6 @@ pub fn part2(input: &str) -> usize {
                 descent.push((row, col+1));
             }
         }
-
-        sum += trial_heads.iter().map(|(row, col)| walked[*row][*col]).sum::<usize>()
     }
     sum
 }
